b857daef01d8f72d812e40cf5f26e821be06eb38
basilissa108 <basilissa.albers@gmail.com>
2025-01-12 16:22:06 +0100

    Add frosted glass styling to input

--- src/app/components/chat-input.tsx
+++ src/app/components/chat-input.tsx
@@ -17,32 +17,34 @@ export const ChatInput = ({
 }: ChatInputProps) => {
   const buttonDisabled = value.length === 0;
   return (
-    <div className="w-3/5 max-w-2xl fixed bottom-10">
-      <label className="flex flex-col gap-2">
-        <span>{label}</span>
-        <textarea
-          rows={4}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && !e.shiftKey) onSubmit();
-          }}
-          className="w-full px-4 py-3 border rounded-2xl resize-none"
-        />
-      </label>
-      <button
-        type="button"
-        onClick={onSubmit}
-        disabled={buttonDisabled}
-        className="absolute bottom-2 right-2 p-0.5 rounded-full focus:ring-2"
-      >
-        {buttonDisabled ? (
-          <BsArrowUpCircle size={22} />
-        ) : (
-          <BsArrowUpCircleFill size={22} />
-        )}
-      </button>
+    <div className="w-screen fixed bottom-0 py-4 flex justify-center bg-white bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 backdrop-saturate-100 backdrop-contrast-100">
+      <div className="w-3/5 max-w-2xl">
+        <label className="flex flex-col gap-2">
+          <span>{label}</span>
+          <textarea
+            rows={4}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) onSubmit();
+            }}
+            className="w-full px-4 py-3 border rounded-2xl bg-transparent resize-none"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={onSubmit}
+          disabled={buttonDisabled}
+          className="absolute bottom-2 right-2 p-0.5 rounded-full focus:ring-2"
+        >
+          {buttonDisabled ? (
+            <BsArrowUpCircle size={22} />
+          ) : (
+            <BsArrowUpCircleFill size={22} />
+          )}
+        </button>
+      </div>
     </div>
   );
 };
